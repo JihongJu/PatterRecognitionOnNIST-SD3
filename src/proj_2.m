f4f56c71d80be00abd07da73f66f441129156bbe
ruoqingsun <sunruoqing@gmail.com>
2016-01-16 19:30:37 +0100

    Feature Selection
    24 features each object

--- src/proj_2.m
+++ src/proj_2.m
@@ -10,24 +10,29 @@ num_labels = 10;          % 10 labels, from 0 to 9
 % Load and Visualize Data
 % Load the images
 fprintf('Loading Data ...\n');
-raw_data = prnist(0:9, 1:100:1000);
-raw_data2 = prnist(0:9, 2:100:1000);
+raw_data = prnist(0:9, 1:10:100);
+raw_data2 = prnist(0:9, 2:10:100);
 
 figure(1);
 show(raw_data);
+
 % x1 = im_features(raw_data,raw_data,'all');
 % x2 = im_features(raw_data2,raw_data2,'all');
 
-x3 = im_mean(raw_data)*datasetm;
-x4 = im_mean(raw_data2)*datasetm;
+% x3 = im_mean(raw_data)*datasetm;
+% x4 = im_mean(raw_data2)*datasetm;
+
+
+x1 = im_features(raw_data,raw_data,'all');
+x2 = im_features(raw_data2,raw_data2,'all');
 
 % labels
 labs = [];
 for i = 1:10
     labs = [labs; (i-1)*ones(10,1)];
 end
-a1 = prdataset(+x3,labs);
-a2 = prdataset(+x4,labs);
+a1 = prdataset(+x1,labs);
+a2 = prdataset(+x2,labs);
 
 % split the whole dataset by 80 %
 [trData, tstData] = gendat(a1,0.8);
@@ -38,7 +43,7 @@ a2 = prdataset(+x4,labs);
 [trData5, tstData5] = gendat(a1,0.8);
 
 %Apply PCA into the trainning set
-u = scalem([], 'variance') * pcam([],0.7)*parzenc;
+u = scalem([], 'variance') * pcam([],8)*parzenc;
 w1 = trData1 * u;
 w2 = trData2 * u;
 w3 = trData3 * u;
@@ -48,7 +53,7 @@ w_all = [w1 w2 w3 w4 w5] * maxc;
 
 % test on tstData
 cls = a2*w_all*labeld;
-tstLabs = a2.nlab - 1;
+tstLabs = a2.nlab-1;
 
 % calculate the error
 corr = cls==tstLabs;
